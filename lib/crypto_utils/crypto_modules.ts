09c17e32349f53efcc03cfa44de64b39848308ed
bguihard <bastien.guihard@superpitch.fr>
2020-03-06 13:18:46 +0100

    It works...

--- lib/crypto_utils/crypto_modules.ts
+++ lib/crypto_utils/crypto_modules.ts
@@ -3,15 +3,13 @@ import * as StringUtils from '../string_utils/string_modules'
 
 /**
  * 
+ * @param hexInput 
  */
 export function hexToBase64(hexInput: string): string {
     const hex = hexInput.toUpperCase();
-    let binBuffer: string = CUtils.hexToBin(hex);
-    binBuffer = CUtils.padding(binBuffer);
+    const binBuffer: string = CUtils.padding(CUtils.hexToBin(hex));
     const chunks = StringUtils.chunkString(binBuffer, 6);
-    if(chunks === null){
-        throw Error("Chunks are null");
-    }
+    if(chunks === null){ throw Error("Chunks are null");}
     let chunksBuffer: string = "";
     chunks.forEach(element => {
         chunksBuffer = chunksBuffer + CUtils.BIN_TO_BASE64[element]
@@ -21,6 +19,8 @@ export function hexToBase64(hexInput: string): string {
 
 /**
  * 
+ * @param hex1 
+ * @param hex2 
  */
 export function fixedHexXor(hex1: string, hex2: string):string {
     if (hex1.length !== hex2.length){
@@ -38,34 +38,34 @@ export function fixedHexXor(hex1: string, hex2: string):string {
 
 /**
  * 
+ * @param inputHex 
+ * @param inputKey 
  */
-export function singleByteXorCipher(inputHex: string, key: string):string {
+export function singleByteXorCipher(inputHex: string, inputKey: number):string {
 
     const hex = inputHex.toUpperCase();
-
-    if (key.length !== 1){
-        throw Error ("Key length 1")
-    };
-    let singleByteCipher = "";
-    for (let i = 0; i < hex.length; i++) {
-        singleByteCipher = singleByteCipher + key
+    const bin: string = CUtils.hexToBin(hex);
+    let binKey = inputKey.toString(2);
+    binKey = "0".repeat(8 - binKey.length) + binKey;
+    if(binKey.length%8 !== 0){
+        throw Error ("8 bytes required 0 - 255");
     }
-
-    return fixedHexXor(hex,singleByteCipher);
+    let cipher = binKey.repeat(bin.length/8);
+    return CUtils.toAscii(CUtils.xor(bin,cipher));
 }
 
 /**
  * 
  */
-export function detectSingleCharXor(hex: string):string {
-    return "1";
-}
+export function repeatingKeyXor(message: string, inputKey: string):string {
+    let buffer = "";
+    for (let i = 0; i < message.length; i++) {
+        const char = message.charAt(i);
+        const keyChar = inputKey.charAt(i%inputKey.length)
+        buffer += CUtils.xorSingleByte(char,keyChar)
+    }
+    return CUtils.binToHex(buffer);
 
-/**
- * 
- */
-export function repeatingKeyXor(hex: string):string {
-    return "1";
 }
 
 /**
--- tests/crypto.test.ts
+++ tests/crypto.test.ts
@@ -1,5 +1,6 @@
 import * as Chai from 'chai';
 import * as Crypto from '../lib/crypto_utils/crypto_modules';
+import * as CUtils from '../lib/crypto_utils/utils';
 
 const should = Chai.should();
 
@@ -10,7 +11,45 @@ describe('Library#crypto', () => {
     });
     it('todo2', () => {
         const test = Crypto.fixedHexXor("686974207468652062756c6c277320657965","1c0111001f010100061a024b53535009181c")
-        //test.should.eql("746865206b696420646f6e277420706c6179");
-        console.log(test);
+        test.should.eql("746865206b696420646f6e277420706c6179".toUpperCase());
+    });
+    it('todo3', () => {
+        const hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+        const test = Crypto.singleByteXorCipher(hex, 88);
+        test.should.eql("Cooking MC's like a pound of bacon");
+        
+    });
+    it('english scoring', () => {
+        const hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+        let arr: string[] = []
+        for (let i = 0; i < 255; i++) {
+            arr.push(Crypto.singleByteXorCipher(hex, i));
+        }
+        const test = CUtils.englishScoring(arr);
+        test.should.eql("Cooking MC's like a pound of bacon");
+    });
+/*
+    it('todo4', () => {
+        let responses: string[] = [];
+        CUtils.TO_TEST.forEach(hex=>{
+        let arr: string[] = [];
+        for (let i = 0; i < 255; i++) {
+            arr.push(Crypto.singleByteXorCipher(hex, i));
+        }
+        responses.push(CUtils.englishScoring(arr));
+        })
+        const test = CUtils.englishScoring(responses);
+        test.should.eql("Now that the party is jumping\n");
+    });*/
+
+    it('todo5', () => {
+        const str = "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal";
+        const res ="0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+        
+        const key = "ICE";
+        const test = Crypto.repeatingKeyXor(str,key);
+        //test.should.eql(res.toUpperCase());
+       
+
     });
 });
